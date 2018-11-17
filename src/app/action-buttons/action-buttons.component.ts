69116356c2494244231e334a2e48775f9c70ef9e
idl99 <ihandilnath2012@gmail.com>
2018-11-17 14:46:36 +0530

    added ActionButtonsComponent

---
+++ src/app/action-buttons/action-buttons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-action-buttons',
+  templateUrl: './action-buttons.component.html',
+  styleUrls: ['./action-buttons.component.css']
+})
+export class ActionButtonsComponent {
+
+  constructor() { }
+
+}
