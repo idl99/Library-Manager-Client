9c8089e266ad16be705353b6b3df7db072063da9
idl99 <ihandilnath2012@gmail.com>
2018-12-02 17:39:31 +0530

    added implementation of ReportComponent

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { LibraryService } from '../shared/services/library.service';
+import { MatTableDataSource, MatSort } from '@angular/material';
+import { LibraryItem } from '../shared/models/LibraryItem';
 
 @Component({
   selector: 'app-report',
@@ -8,8 +11,23 @@ import { Component, OnInit } from '@angular/core';
 export class ReportComponent {
 
   displayedColumns: string[] = ['isbn', 'title', 'borrowedOn', 'overdueBy', 'fee'];
-  dataSource = [];
+  dataSource = new MatTableDataSource<LibraryItem>();
 
-  constructor() { }
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private libraryService: LibraryService) {
+    this.initializeDataSource();
+  }
+
+  private initializeDataSource(): void {
+    this.libraryService.getReport('10/12/2018').subscribe(
+      success => {
+        this.dataSource.data = success['items'];
+        console.log(this.dataSource.data);
+        this.dataSource.sort = this.sort;
+        console.log(this.dataSource.data);
+      }
+    );
+  }
 
 }
