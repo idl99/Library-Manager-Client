cfd370d93422306e68dba1d204bd55dbedd8a2d0
idl99 <ihandilnath2012@gmail.com>
2018-12-06 21:23:04 +0530

    added ProgressSpinnerDialogComponent implementation for all CRUD methods

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,48 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Book } from '../shared/models/Book';
-import { MatDialog, MatTableDataSource } from '@angular/material';
+import { MatDialog, MatTableDataSource, MatDialogRef } from '@angular/material';
 import { AddItemDialogComponent } from '../forms/add-item-dialog/add-item-dialog.component';
 import { LibraryService } from '../shared/services/library.service';
 import { LibraryItem } from '../shared/models/LibraryItem';
 import { Dvd } from '../shared/models/Dvd';
 import { Observable } from 'rxjs';
+import { ProgressSpinnerDialogComponent } from '../progress-spinner-dialog/progress-spinner-dialog.component';
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.css']
 })
-export class ItemListComponent {
+export class ItemListComponent implements OnInit {
+
 
 
   private dataSource: MatTableDataSource<LibraryItem>;
   private columnsToDisplay: String[] = ['Availability', 'Type', 'ISBN', 'title', 'section', 'actionBtns', 'availableOn'];
+  progressSpinnerDialogRef: MatDialogRef<ProgressSpinnerDialogComponent, any>;
 
 
   constructor(public dialog: MatDialog, private libraryService: LibraryService ) {
-    // this.dataSource = new LibraryItemDataSource(libraryService);
     this.dataSource = new MatTableDataSource();
-    this.initializeDatasource();
   }
 
+  ngOnInit(): void {
+
+    // Setting timeout to allow enough time to show animation
+    setTimeout(() => {
+      this.startProgressSpinner('Getting Items', 'Please wait while we fetch the Item List.');
+      setTimeout( () => {
+        this.libraryService.getAllItems().subscribe(
+          (response) => {
+            this.dataSource.data = response as LibraryItem[];
+          },
+          (err) => {
+            alert(err);
+          },
+          () => {
+            this.finishProgressSpinner();
+        });
+      }, 2500);
+    });
+
+  }
 
   public applyFilter(filterString: String): void {
     this.dataSource.filter = filterString.trim().toLowerCase();
-  }
-
-
-  private initializeDatasource(): void {
-    this.libraryService.getAllItems().subscribe(
-      response => {
-        this.dataSource.data = response as LibraryItem[];
-      },
-      err => alert(err)
-      );
-  }
-
-
-  private isAvailable(item: LibraryItem): boolean {
-    return item.getCurrentReader() === null ? true : false;
   }
 
 
@@ -56,81 +62,13 @@ export class ItemListComponent {
   }
 
 
-  // method invoked to open Add Item Dialog
-  private onAddItem(choice: String): void {
-    this.dialog.open(AddItemDialogComponent, {
-      width: '600px',
-      data: {
-        itemType: choice
-      }
-    });
-    this.dialog._afterAllClosed.subscribe(() => this.initializeDatasource()
-    );
-  }
-
-
-  private onDelete(item: LibraryItem): void {
-    let observable: Observable<String>;
-    if (item instanceof Book) {
-      observable = this.libraryService.deleteBook(item.getIsbn());
-    } else if (item instanceof Dvd) {
-      observable = this.libraryService.deleteDvd(item.getIsbn());
-    }
-    observable.subscribe(
-      success => {
-        this.initializeDatasource();
-        alert(success);
-      },
-      err => {
-        alert(err);
-      }
-    );
-  }
-
-
-  // method to open Borrow Item Dialog
-  private onBorrow(libraryItem: LibraryItem): void {
-    const readerId: String = window.prompt('Enter Reader Id: ');
-    if (readerId === null) {
-      return;
-    }
-    this.libraryService.borrowItem(libraryItem, readerId, new Date(Date.now()).toLocaleDateString('en-gb')).subscribe(
-      success => {
-        this.initializeDatasource();
-        alert(success);
-      },
-      err => alert(err)
-    );
-  }
-
-
-  private onReturn(libraryItem: LibraryItem): void {
-    this.libraryService.returnItem(libraryItem).subscribe(
-      success => {
-        this.initializeDatasource();
-        alert(success);
-      },
-      err => {
-        alert(err);
-      }
-    );
-  }
-
-
-  private onReserve(libraryItem: LibraryItem): void {
-    const readerId = window.prompt('Enter Reader Id: ');
-    if (readerId == null) {
-      alert('Reader Id is required. Try again');
-    } else {
-      this.libraryService.reserveItem(libraryItem, readerId).subscribe(
-        success => alert(success),
-        err => alert(err)
-      );
-    }
+  private isAvailable(item: LibraryItem): boolean {
+    return item.getCurrentReader() === null ? true : false;
   }
 
 
   private getAvailableOn(item: LibraryItem): String {
+
     if (this.isAvailable(item)) {
       return 'Available Now';
     } else {
@@ -138,8 +76,8 @@ export class ItemListComponent {
       const day: number = parseInt(item.getBorrowedOn()['day'], null);
       const month: number = parseInt(item.getBorrowedOn()['month'], null);
       const year: number = parseInt(item.getBorrowedOn()['year'], null);
-
       const date: Date = new Date(year, month - 1, day);
+
       if (item instanceof Book) {
         date.setDate(date.getDate() + 7);
         return date.toLocaleDateString('en-gb');
@@ -151,5 +89,143 @@ export class ItemListComponent {
     }
 
   }
+
+
+  // method invoked to open Add Item Dialog
+  private onAddItem(choice: String): void {
+    const addItemDialogRef = this.dialog.open(AddItemDialogComponent, {
+      width: '600px',
+      data: {
+        itemType: choice
+      }
+    });
+    addItemDialogRef.afterClosed().subscribe(
+      (response) => {
+        if (response === 'Ok') {
+          this.ngOnInit();
+        }
+      }
+    );
+  }
+
+
+  private onDelete(item: LibraryItem): void {
+
+    let observable: Observable<String>;
+
+    if (item instanceof Book) {
+      observable = this.libraryService.deleteBook(item.getIsbn());
+    } else if (item instanceof Dvd) {
+      observable = this.libraryService.deleteDvd(item.getIsbn());
+    }
+
+    this.startProgressSpinner('Deleting Item', 'Please wait while we delete this item from the database.');
+
+    setTimeout( () => {
+      observable.subscribe(
+        (success) => {
+          this.finishProgressSpinner();
+          this.ngOnInit();
+          alert(success);
+        },
+        (err) => {
+          this.finishProgressSpinner();
+          alert(err);
+        });
+    }, 2500);
+
+  }
+
+
+  private onBorrow(libraryItem: LibraryItem): void {
+
+    const readerId: String = window.prompt('Enter Reader Id: ');
+    if (readerId === null) {
+      return;
+    }
+
+    this.startProgressSpinner('Borrowing Item', 'Please wait while we process your Item Borrowal.');
+
+    setTimeout(() => {
+      this.libraryService.borrowItem(libraryItem, readerId, new Date(Date.now()).toLocaleDateString('en-gb')).subscribe(
+        success => {
+          this.progressSpinnerDialogRef.close();
+          this.ngOnInit();
+          alert(success);
+        },
+        err => {
+          alert(err);
+        }
+      );
+    }, 2500);
+
+  }
+
+
+  private onReturn(libraryItem: LibraryItem): void {
+
+    this.startProgressSpinner('Returning Item', 'Please wait while we process this Item Return.');
+
+    setTimeout(() => {
+      this.libraryService.returnItem(libraryItem).subscribe(
+        success => {
+          this.finishProgressSpinner();
+          this.ngOnInit();
+          alert(success);
+        },
+        err => {
+          this.finishProgressSpinner();
+          alert(err);
+        }
+      );
+    }, 2500);
+
+  }
+
+
+  private onReserve(libraryItem: LibraryItem): void {
+
+    const readerId = window.prompt('Enter Reader Id: ');
+
+    if (readerId == null) {
+      alert('Reader Id is required. Try again');  
+    } else {
+
+      this.startProgressSpinner('Reserving Item', 'Please wait while we reserve this Item for you.');
+
+      setTimeout(() => {
+        this.libraryService.reserveItem(libraryItem, readerId).subscribe(
+          success => {
+            alert(success);
+          },
+          err => {
+            alert(err);
+          },
+          () => {
+            this.finishProgressSpinner();
+          }
+        );
+      }, 2500);
+
+    }
+  }
+
+  private startProgressSpinner(title: String, message: String) {
+    this.progressSpinnerDialogRef = this.dialog.open(ProgressSpinnerDialogComponent, {
+      panelClass: 'transparent',
+      disableClose: true,
+      data: {
+        title: title,
+        message: message
+      }
+    });
+  }
+
+  private finishProgressSpinner() {
+    if (this.progressSpinnerDialogRef !== undefined) {
+      this.progressSpinnerDialogRef.close();
+    }
+  }
+
 
 }
