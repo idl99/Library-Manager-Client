f57d653c9e1474d6bff130ade06db386090332ea
idl99 <ihandilnath2012@gmail.com>
2018-12-08 18:52:48 +0530

    added button and method to logout

--- src/app/app-navigation/app-navigation.component.ts
+++ src/app/app-navigation/app-navigation.component.ts
@@ -2,20 +2,19 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { LoginService } from '../shared/services/login.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './app-navigation.component.html',
-  styleUrls: ['./app-navigation.component.css'],
+  styleUrls: ['./app-navigation.component.css']
 })
 export class AppNavigationComponent {
-
   // Observable to detect if screen size is mobile - if so sideNav will be collapsed
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private loginService: LoginService) {}
 
 }
